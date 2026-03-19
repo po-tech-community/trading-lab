5639a715940b817bc4c97a847dc533e4453084f4
Quan Huynh <quanhuynh364@gmail.com>
2026-03-18 22:41:36 -0400

    add GET /users/me protected endpoint

--- back-end/src/users/users.module.ts
+++ back-end/src/users/users.module.ts
@@ -12,13 +12,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  controllers: [],
+  controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
